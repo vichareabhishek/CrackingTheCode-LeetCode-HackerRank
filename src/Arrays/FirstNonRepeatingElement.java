0dd0f11e11d1fb0df2572952a2225b4487983acc
abhishekk6690 <abhishekk6690@gmail.com>
2017-10-03 00:01:10 -0400

    updating with more problems

---
+++ src/Arrays/FirstNonRepeatingElement.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+public class FirstNonRepeatingElement {
+	static char[] ch = new char[256];
+
+	 static void getCount(String s){
+	
+		for(int i=0; i<s.length(); i++){
+			ch[s.charAt(i)]++;
+		}
+	}
+	 static int getNonRepeatingIndex(String s){
+		getCount(s);
+		
+		int index =-1;
+		for(int i=0; i<s.length(); i++){
+			if(ch[s.charAt(i)]==1){
+				index=i;
+				break;
+			}
+			
+		}
+		return index;
+	}
+public static void main(String[] args) {
+		
+		String a ="geeksforgeeks";
+		
+		int index = getNonRepeatingIndex(a);
+		if(index==1){
+			System.out.println("Noting repeated");
+		
+		}else{
+			System.out.println(a.charAt(index));
+		}
+	}
+	
+}
