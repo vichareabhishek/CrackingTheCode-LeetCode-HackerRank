7155e9367a1c8b61831f13e06c460ee14d81d792
abhishekk6690 <abhishekk6690@gmail.com>
2017-10-11 02:29:32 -0400

    updating with more problems

---
+++ src/Tree/MergeBinaryTree.java
@@ -0,0 +1,43 @@
+package Tree;
+
+public class MergeBinaryTree {
+	public static class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	  }
+	 public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+	        
+		 if(t1==null && t2==null){
+			 return null;
+		 }
+		 
+		 TreeNode newNode = new TreeNode(-1);
+		 if(t1==null){
+	            
+	        	newNode.val =t2.val;
+	        	newNode.left = mergeTrees(null,t2.left);
+	        	newNode.right = mergeTrees(null, t2.right);
+	        	
+	        	
+	        }
+		 else if(t2==null){
+	        	 
+	        	newNode.val =t1.val;
+	        	newNode.left = mergeTrees(t1.left,null);
+	        	newNode.right = mergeTrees(t1.right, null);
+	        }
+		 else{
+			 newNode.val = t1.val+t2.val;
+	        newNode.left = mergeTrees(t1.left, t2.left);
+	        newNode.right = mergeTrees(t1.right, t2.right);
+		 }
+	        return newNode;
+		 
+	    }
+}
+
+
+
+  
